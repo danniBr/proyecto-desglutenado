67616e6046b03c12668c9882196966d72344b8f9
danniBr <danitza.br@gmail.com>
2018-11-06 21:15:05 -0300

    questions CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :recipes
+  resources :questions
   devise_for :users, controllers: {
       registrations: 'users/registrations'
    }
   get 'pages/index'
+  
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
