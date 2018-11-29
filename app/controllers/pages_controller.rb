6e8b575f4ca0234b8bb742585b4a7888ba1148d7
danniBr <danitza.br@gmail.com>
2018-11-29 19:33:29 -0300

    homepage recipes integration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
     @recipes = Recipe.order('created_at desc').limit(4)
+    @locations = Location.order('created_at desc').limit(5)
   end
 end
