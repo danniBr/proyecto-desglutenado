95f834a3004c62a112d3802c62df8a4c5a70a542
danniBr <danitza.br@gmail.com>
2018-12-06 16:45:04 -0300

    rollback to older commit status

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery/dist/jquery
 //= require bootstrap/dist/js/bootstrap
+//= require rails-ujs
 //= require turbolinks
 //= require underscore/underscore
 //= require gmaps/google
