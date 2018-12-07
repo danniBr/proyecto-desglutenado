c43477f4c4bc807afdea4d45fa517315cd6759ec
danniBr <danitza.br@gmail.com>
2018-12-07 15:02:19 -0300

    routes problem fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'locations/findaddress', to: 'locations#findaddress'
-  
-  devise_for :users, controllers: {
-      registrations: 'users/registrations'
-   }, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
+
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks' }
+
   resources :recipes do
     resources :likes
   end
@@ -15,12 +15,12 @@ Rails.application.routes.draw do
   resources :locations
   resources :questions do
     resources :comments
-  end 
-  
+  end
+
   delete 'questions/:question_id/comments/:id', to: 'comments#destroy', as: 'delete_comment'
-  
+
   get 'pages/index'
-  
+
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
